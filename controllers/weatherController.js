b82db63d66ed3835ccd5e58005e4bbbd898b65eb
Piotrek <piotrek120017@gmail.com>
2018-04-20 13:01:54 +0200

    poprawki w zwrqacanych danych i zmiana api

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -1,30 +1,39 @@
 'use strict';
+var weather = require('openweather-apis');
 
-const OpenWeatherMapHelper = require("openweathermap-node");
+weather.setLang('pl');
 
-const helper = new OpenWeatherMapHelper(
-    {
-        APPID: '609191e92beb5b9b8d10c2124c026211',
-        units: "metric"
-    }
-);
 
   exports.getCities = function(req, res) {
-    var cities = [{id: 1, name: "Warszawa"}, {id: 2, name: "Łódz"}, {id:3, name: "Płock"}];
+    var cities = [{id: 1, name: "Warszawa"}, {id: 2, name: "Łódź"}, {id:3, name: "Płock"}, {id:4, name: "dupacycki"}];
     res.json(cities);
   }
 
   exports.getWeather = function(req, res) {
-    helper.getCurrentWeatherByCityName(req.params.city, (err, currentWeather) => {
-      if(err){
-          console.log(err);
-          res.sendStatus(404);
-      }
-      else{
-        console.log(currentWeather);
-        var weather = {};
-        Object.assign(weather, currentWeather.weather[0],  currentWeather.main, currentWeather.wind, {visibility: currentWeather.visibility});
-        res.json(weather);
-      }
-    });
+      console.log(req.params.city);
+
+      weather.setCity(req.params.city);
+      weather.setUnits("metric");
+
+      weather.setAPPID("609191e92beb5b9b8d10c2124c026211");
+
+      weather.getAllWeather(function (err, currentWeather) {
+          if(err){
+              console.log(err);
+              res.sendStatus(404);
+          }
+          else {
+              console.log(currentWeather);
+              if (currentWeather.cod !=200){
+                  res.sendStatus(404);
+              }else {
+                  var weather = {};
+                  Object.assign(weather, currentWeather.weather[0], currentWeather.main, {
+                      speed: currentWeather.wind.speed,
+                      cod: currentWeather.cod});
+                  console.log(weather);
+                  res.json(weather);
+              }
+          }
+      });
   };
